f67aa27f5d1fe161dcf15577587858e3b61d957c
serenitysmith <serenitysmith24@gmail.com>
2023-09-14 20:04:23 -0400

    notes

--- src/Box.js
+++ src/Box.js
@@ -63,20 +63,32 @@ const toggleBox = () => {
 
 
 // ### **Local State**
-
+// This code is for a simple React function component named "MyComponent" which uses the React Hook "useState". Let's break it down:
 // In React, local state refers to the state that is managed and used within a specific component. It is distinct from global state managed by state management libraries like React Context. Local state is typically used when the state data is relevant only to a specific component and does not need to be shared with other components.
 
 // To manage local state in a functional component, you can use the **`useState`** hook provided by React. The **`useState`** hook allows you to declare state variables and their corresponding update functions. Here's an example:
-
-// ```jsx
+// ```
+// This line imports the React library and one of its Hooks called 'useState'. 'useState' allows the component to have state variables
+// // ```jsx
 // import React, { useState } from 'react';
-
+// This line is declaring a functional component called MyComponent.
 // function MyComponent() {
 // const [count, setCount] = useState(0);
+// Using the useState hook, a new state variable 'count' is being declared which is initially set to 0. The function 'setCount' is used to update the state variable 'count'.
+
+
+
+
+
+// Here an arrow function named 'increment' is declared. This function's job is to increase the 'count' state variable by 1 every time it gets called.
 
 // const increment = () => {
 // setCount(count + 1);
 // };
+
+
+
+// The 'return' keyword is used to specify what the component should render. Here it renders a 'div' containing a paragraph tag 'p' and a button. This block says to display the current state of 'count', and a button that when clicked on, will call the 'increment' function and increase the count by 1.
 
 // return (
 // <div>
@@ -86,7 +98,9 @@ const toggleBox = () => {
 // );
 // }
 // ```
+// Finally, the definition of the 'MyComponent' function is closed using the '}' character.
 
+// So in simple terms, this is a React component that displays a count and a button. Every time we click on the button, the count increases by 1.
 // In this example, the **`useState`** hook is used to declare a local state variable called **`count`** with an initial value of **`0`**. The hook returns an array with two elements: the current value of the state variable (**`count`**) and a function (**`setCount`**) to update the state.
 
 // The **`increment`** function is an event handler that calls **`setCount`** with the updated value of **`count + 1`** when the button is clicked. This triggers a re-render of the component with the updated state, causing the count value to be displayed in the UI.
